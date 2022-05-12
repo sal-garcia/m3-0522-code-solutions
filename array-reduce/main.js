d3c5437c72e156fe8188cc23c46ea083fd14d662
sal garcia <salgarcia930@gmail.com>
2022-05-12 03:49:23 +0000

    array reduce completed

---
+++ array-reduce/main.js
@@ -0,0 +1,44 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+
+const sum = numbers.reduce((previousValue,currentValue)=>{
+  return previousValue+currentValue
+})
+console.log('sum',sum);
+
+const product =numbers.reduce((previousValue,currentValue) => {
+  return previousValue * currentValue
+},1)
+console.log('product',product);
+
+const balance = account.reduce((previousValue,currentValue)=>{
+  if(currentValue.type==='deposit'){
+    previousValue+= currentValue.amount// adds all the deposits to previousValue
+  }else{
+    previousValue -= currentValue.amount// subtracts all of the withdrawls to previousValue
+  }
+  return previousValue
+},0);
+console.log('balance',balance)
+
+const composite = traits.reduce((previousValue,currentValue)=>{
+  return Object.assign(previousValue,currentValue);
+},{});
+
+console.log('composite',composite)
