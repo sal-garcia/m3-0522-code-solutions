c039166b07f1a1647132c5dcb804b557b122caf9
sal garcia <salgarcia930@gmail.com>
2022-05-18 04:28:44 +0000

    updated

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-console.log(React.createElement(
+const Element = React.createElement(
   'h1',
   [null],
   ['Hello, React!']
-))
+)
 
 const container = document.querySelector('#root');
 
