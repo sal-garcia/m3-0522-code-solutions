acd8560a7c509814ee36ccb9c1387a2c3b4b012d
sal garcia <salgarcia930@gmail.com>
2022-06-02 02:03:23 +0000

    react hot button componenet refactored

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -12,33 +12,35 @@ class HotButton extends React.Component {
   }
 
   render() {
+    let btnClass;
     if (this.state.clicks < 3){
 
-       return <button className="button" onClick={this.btnClicks}>Hot Button</button>
+        btnClass="button"
     }
     else if (this.state.clicks < 6){
 
-    return <button className="darkPurple button" onClick={this.btnClicks}>Hot Button</button>
+     btnClass="darkPurple button"
     }
     else if (this.state.clicks < 9){
 
-       return <button className="purple button" onClick={this.btnClicks}>Hot Button</button>
+        btnClass="purple button"
     }
     else if (this.state.clicks < 12){
 
-       return <button className="red button" onClick={this.btnClicks}>Hot Button</button>
+        btnClass="red button"
     }
     else if (this.state.clicks < 15){
-       return <button className="orange button" onClick={this.btnClicks}>Hot Button</button>
+        btnClass="orange button"
     }
     else if (this.state.clicks < 18){
 
-       return <button className="yellow button" onClick={this.btnClicks}>Hot Button</button>
+        btnClass="yellow button"
     }
     else{
 
-    return <button className="white button" onClick={this.btnClicks}>Hot Button</button>
+     btnClass="white button"
     }
+    return <button className={`${btnClass}`} onClick={this.btnClicks}>Hot Button</button>
   }
 }
 export { HotButton };
